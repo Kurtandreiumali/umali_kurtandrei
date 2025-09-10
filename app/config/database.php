afb7554d7a565d697e5fd6f2e458a82c986c89bf
Kurt Andrei Umali <andreicaretas@gmail.com>
2025-09-10 20:57:29 +0800

    Latest Commit

--- app/config/database.php
+++ app/config/database.php
@@ -59,11 +59,11 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
 $database['main'] = array(
     'driver'	=> 'mysql',
-    'hostname'	=> 'localhost',
+    'hostname'	=> 'sql12.freesqldatabase.com',
     'port'		=> '3306',
-    'username'	=> 'root',
-    'password'	=> '',
-    'database'	=> 'mockdata',
+    'username'	=> 'sql12798038',
+    'password'	=> 'pRye6fENI6',
+    'database'	=> 'sql12798038',
     'charset'	=> 'utf8mb4',
     'dbprefix'	=> '',
     // Optional for SQLite
