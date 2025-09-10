13823f2bd3fdd1f8042ef170a9d9cac0527f88fc
Kurt Andrei Umali <andreicaretas@gmail.com>
2025-09-10 16:20:56 +0800

    First Commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '38fe3639e706b91b0678ad1ee4e85931dbc8de4d',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '38fe3639e706b91b0678ad1ee4e85931dbc8de4d',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
